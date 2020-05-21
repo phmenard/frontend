77a35a3af6a792c79635694bac0a86f6d2ea1cbc
unknown <clmonitor2013@gmail.com>
2020-05-21 00:34:05 -0400

    put styling together

---
+++ silent-auction/src/components/MainContainer.js
@@ -0,0 +1,21 @@
+import '../css/index.css';
+import React, { useState } from 'react';
+
+import AuctionList from './ActionList';
+
+import {Route} from 'react-router-dom';
+
+const MainContainer = props => {
+
+    return (
+        
+        <div className = "mainContainer">
+           Hellow world!!      
+
+        </div>
+       
+
+    );
+}
+
+export default MainContainer;
