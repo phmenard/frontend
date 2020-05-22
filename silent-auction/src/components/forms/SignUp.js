f2fdb23a6b76b3dc29c1f6d7c8840870163d82ea
unknown <clmonitor2013@gmail.com>
2020-05-22 00:50:31 -0400

    updated styles

--- silent-auction/src/components/forms/SignUp.js
+++ silent-auction/src/components/forms/SignUp.js
@@ -2,13 +2,11 @@ import React from 'react';
 import '../../css/index.css';
 import { Link } from 'react-router-dom';
 
-
-
 const SignupCard = props => {
   
   return (
     
-    <div className="auctionCard" >
+    <div className="signUpCard" >
     
     <h1>Create Auction</h1>
     <p>Hellow World !!</p>
